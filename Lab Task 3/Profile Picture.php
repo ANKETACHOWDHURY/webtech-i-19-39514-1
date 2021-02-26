9625543fd8dc808641c807586f6a96423b876252
Anketa <chowdhuryanketa@gmail.com>
2021-02-26 19:58:01 +0600

    lab 3

---
+++ Lab Task 3/Profile Picture.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<fieldset style="width: 500px">
+<legend>PROFILE PICTURE</legend>
+<form action="Picture_Upload.php" method="post" enctype="multipart/form-data">
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <hr>
+  
+  <input type="submit" value="Submit" name="submit">
+</form>
+</fieldset>
+
+</body>
+</html>
